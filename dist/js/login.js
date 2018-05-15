15d48acea042b3744b82abe6e2945c2a69587c0e
Wayne Chu <waynechu@waynechu.cn>
2018-05-16 00:36:38 +0800

    '完成结果页的逻辑和展示'

--- dist/js/login.js
+++ dist/js/login.js
@@ -1,1 +1,1 @@
-webpackJsonp([1],{35:function(o,n,e){var l=e(36),r=e(2);l("body").html("HELLO LOGIN for jquery"),console.log("hello login"),r.goHome()},36:function(o,n){o.exports=window.jQuery}},[35]);
+webpackJsonp([1],{38:function(o,n,e){var l=e(39),r=e(2);l("body").html("HELLO LOGIN for jquery"),console.log("hello login"),r.goHome()},39:function(o,n){o.exports=window.jQuery}},[38]);
