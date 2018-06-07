c724c29ecb736231b92c7e9a1c4e0db6f958e94c
Wayne Chu <waynechu@waynechu.cn>
2018-06-07 12:53:16 +0800

    完成首页banner的展示

--- src/page/index/index.js
+++ src/page/index/index.js
@@ -7,4 +7,21 @@
 require('./index.css');
 require('page/common/nav/index.js');
 require('page/common/header/index.js');
+require('util/slider/index.js');
+
+var templateBanner = require('./banner.string');
 var _mm = require('util/mm.js');
+
+$(function () {
+    // 渲染banner的html
+    var bannerHtml = _mm.renderHtml(templateBanner);
+    $('.banner-container').html(bannerHtml);
+    // 初始化banner.  link -> http://basicslider.com/
+    $('.banner').bjqs({
+        animtype: 'fade',
+        height: 370,
+        width: 830,
+        nexttext: '<',
+        prevtext: '>'
+    });
+});
---
+++ src/util/slider/index.js
@@ -0,0 +1,8 @@
+/**
+ * @author waynechu1996@gmail.com
+ * Created 2018-06-07 10:10
+ */
+"use strict";
+
+require('./bjqs.css');
+require('./bjqs-1.3.min.js');
